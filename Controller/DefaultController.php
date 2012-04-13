1e2396400ffea25497769b8db80e88fcf475f74f
Ilan Cohen <ilanco@gmail.com>
2012-04-13 13:03:52 +0000

    prefix route names

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="ilan_bootstrap_homepage")
      * @Template()
      */
     public function indexAction()
